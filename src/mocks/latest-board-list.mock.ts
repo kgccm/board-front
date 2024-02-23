92b26397d67e4041c368c70b201a2e1685965984
kgccm <990720wlgns@naver.com>
2024-02-24 01:41:53 +0900

    01:41

---
+++ src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,56 @@
+import exp from "constants";
+import { BoardListItem } from "types/interface";
+
+const latestBoardListMock: BoardListItem[] = [
+    {
+        "boardNumber": 1,
+        "title": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        "content": "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 ...",
+        "boardTitleImage": 'https://www.sisaon.co.kr/news/photo/202302/147321_173869_555.jpg',
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDatetime": "2023.08.18. 00:54:27",
+        "writerNickname": "안녕하세요나는주코야키",
+        "writerProfileImage": 'https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg',
+    },
+    {
+        "boardNumber": 1,
+        "title": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        "content": "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 ...",
+        "boardTitleImage": null,
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDatetime": "2023.08.18. 00:54:27",
+        "writerNickname": "안녕하세요나는주코야키",
+        "writerProfileImage": null,
+    },
+    {
+        "boardNumber": 1,
+        "title": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        "content": "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 ...",
+        "boardTitleImage": null,
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDatetime": "2023.08.18. 00:54:27",
+        "writerNickname": "안녕하세요나는주코야키",
+        "writerProfileImage": null,
+    },
+    {
+        "boardNumber": 1,
+        "title": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+        "content": "오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 ...",
+        "boardTitleImage": null,
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDatetime": "2023.08.18. 00:54:27",
+        "writerNickname": "안녕하세요나는주코야키",
+        "writerProfileImage": null,
+    }
+
+];
+
+export default latestBoardListMock;
