7fc305f3603b6b666c805616758319386f5e146b
kgccm <990720wlgns@naver.com>
2024-02-25 02:54:08 +0900

    mock

--- src/components/Top3Item/index.tsx
+++ src/components/Top3Item/index.tsx
@@ -1,29 +1,49 @@
 import React from 'react'
 import './style.css';
+import defaultProfileImage from 'assets/image/default-profile-image.png';
+import { BoardListItem } from 'types/interface';
+import { useNavigate } from 'react-router-dom';
+
+interface Props{
+    top3ListItem : BoardListItem
+}
 
 //          Component: Top 3 List Item 컴포넌트          //
-export default function Top3Item() {
-// ∙
+export default function Top3Item({top3ListItem}: Props) {
+
+     //          properties          //
+     const { boardNumber, title, content, boardTitleImage } = top3ListItem;
+     const { favoriteCount, commentCount, viewCount } = top3ListItem;
+     const { writeDatetime, writerNickname, writerProfileImage } = top3ListItem;
+
+    //          function : navigate 함수          //
+    // const navigator = useNavigate();
+    
+    //          event Handler: 게시물 아이템 클릭 이벤트 처리 함수          //
+    const onClickHandler = () => {
+        // navigator(boardNumber);
+    }
+
     //          render: Top 3 List Item 컴포넌트 렌더링          //
   return (
-    <div className='top-3-list-item'>
+    <div className='top-3-list-item' style={{backgroundImage:`url(${boardTitleImage})`}} onClick={onClickHandler}>
         <div className='top-3-list-item-main-box'>
             <div className='top-3-list-item-top'>
                 <div className='top-3-list-item-profile-box'>
-                    <div className='top-3-list-item-profile-image' style={{backgroundImage: `url()`}}></div>
+                    <div className='top-3-list-item-profile-image' style={{backgroundImage: `url(${writerProfileImage ? writerProfileImage : defaultProfileImage})`}}></div>
                 </div>
                 <div className='top-3-list-item-write-box'>
-                    <div className='top-3-list-item-nickname'>{'나는 개보지'}</div>
-                    <div className='top-3-list-item-write-date'>{'2024. 02. 25'}</div>
+                    <div className='top-3-list-item-nickname'>{writerNickname}</div>
+                    <div className='top-3-list-item-write-date'>{writeDatetime}</div>
                 </div>
             </div>
             <div className='top-3-list-item-middle'>
-                <div className='top-3-list-item-title'>{'노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현...'}</div>
-                <div className='top-3-list-item-content'>{'노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현노무현...'}</div>
+                <div className='top-3-list-item-title'>{title}</div>
+                <div className='top-3-list-item-content'>{content}</div>
             </div>
             <div className='top-3-list-item-bottom'>
                 <div className='top-3-list-item-counts'>
-                    {`댓글 0 ∙ 좋아요 0 ∙ 조회수 0`}
+                    {`댓글 ${commentCount} ∙ 좋아요 ${favoriteCount} ∙ 조회수 ${viewCount}`}
                 </div>
             </div>
         </div>
