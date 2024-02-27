f63a111ff498e476db7e371e967e067c699c703d
kgccm <990720wlgns@naver.com>
2024-02-28 02:35:02 +0900

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import { latestBoardListMock, top3BoardListMock, commentListMock, favoriteListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+
+import Footer from 'layouts/Footer';
 
 function App() {
 
-  const [value, setValue] = useState<string>('');
-
   return (
-   <>
-    <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa'/>
-   </>
+    <>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './style.css';
+
+//          component: Footer Layout          //
+export default function Footer() {
+
+    //          event handler: 인스타 아이콘 버튼 클릭 이벤트 처리          //
+    const onInstaIconButtonClickHandler = () => {
+        window.open('https://www.instagram.com');
+    }
+    
+    //          event handler: 네이버 아이콘 버튼 클릭 이벤트 처리          //
+    const onNaverBlogIconButtonClickHandler = () => {
+        window.open('https:/blog.naver.com');
+    }
+
+    //          render: Footer Layout 렌더링          //
+    return (
+        <div id='footer'>
+            <div className='footer-container'>
+                <div className='footer-top'>
+                    <div className='footer-logo-box'>
+                        <div className='icon-box'>
+                            <div className='icon logo-light-icon'></div>
+                        </div>
+                        <div className='footer-logo-text'>{'Hoons Board'}</div>
+                    </div>
+                    <div className='footer-link-box'>
+                        <div className='footer-email-link'>{'990720wlgns@naver.com'}</div>
+                        <div className='icon-button' onClick={onInstaIconButtonClickHandler}>
+                            <div className='icon insta-icon'></div>
+                        </div>
+                        <div className='icon-button' onClick={onNaverBlogIconButtonClickHandler}>
+                            <div className='icon naver-blog-icon'></div>
+                        </div>
+                    </div>
+                </div>
+                <div className='footer-bottom'>
+                    <div className='footer-copyright'>{'Copyright © 2022 Jukoyakki. All Rights Reserved.'}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
