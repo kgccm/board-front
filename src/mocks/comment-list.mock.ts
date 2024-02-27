72329b7f09e59d8606dda54fdf66df9b77a16dad
kgccm <990720wlgns@naver.com>
2024-02-28 01:23:05 +0900

    inputbox

---
+++ src/mocks/comment-list.mock.ts
@@ -0,0 +1,22 @@
+const commentListMock = [
+    {
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null,
+        "writeDatetime": "3분전",
+        "content": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+    },
+    {
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null,
+        "writeDatetime": "3분전",
+        "content": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+    },
+    {
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null,
+        "writeDatetime": "3분전",
+        "content": "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데",
+    }
+];
+
+export default commentListMock;
