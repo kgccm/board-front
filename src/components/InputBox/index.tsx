fd0526e7340034dcb8f8efccbdba514a924fb34d
kgccm <990720wlgns@naver.com>
2024-03-02 03:50:44 +0900

    0302

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Dispatch, forwardRef, KeyboardEvent } from 'react';
+import { ChangeEvent, forwardRef, KeyboardEvent } from 'react';
 import './style.css';
 
 //          interface: Input Box 컴포넌트 Properties          //
@@ -7,7 +7,7 @@ interface Props {
     type: 'text' | 'password';
     placeholder: string;
     value: string;
-    setValue: Dispatch<React.SetStateAction<string>>;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
     error: boolean;
 
     icon?: 'eye-light-off-icon' | 'eye-light-on-icon' | 'expand-right-light-icon';
@@ -23,14 +23,9 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
 
     //          state: properties          //
     const { label, type, placeholder, value, error, icon, message } = props;
-    const { setValue, onButtonClick, onKeyDown } = props;
+    const { onChange, onButtonClick, onKeyDown } = props;
 
-    //          event handler: input 값 변경 이벤트 처리 함수          //
-    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        setValue(value);
-    };
-
+  
     //          event handler: input 키 이벤트 처리 함수          //
     const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
         if(!onKeyDown) return;
@@ -42,7 +37,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
         <div className='inputbox'>
             <div className='inputbox-label'>{label}</div>
             <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
-                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler} />
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChange} onKeyDown={onKeyDownHandler} />
                 {onButtonClick !== undefined && 
                     <div className='icon-button' onClick={onButtonClick}>
                         {icon !== undefined && 
@@ -51,9 +46,6 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
                     </div>
 
                 }
-                <div className='icon-button'>
-                    <div className='icon eye-light-off-icon'></div>
-                </div>
             </div>
             {message !== undefined && <div className='inputbox-message'>{message}</div>}
         </div>
