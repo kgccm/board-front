f6563b16115d6ae6ed56d48b650daa5a2f5c0e78
kgccm <990720wlgns@naver.com>
2024-03-05 00:56:58 +0900

    0305

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -112,6 +112,7 @@ export default function Header() {
     //          event handler: 로그아웃 버튼 클릭 이벤트 처리 함수          //
     const onSignOutbuttonClickHandler = () => {
       resetLoginUser();
+      setCookie('accessToken', '', {path: MAIN_PATH(), expires: new Date()});
       navigate(MAIN_PATH());
     };
     //          event handler: 로그인 버튼 클릭 이벤트 처리 함수          //
@@ -165,6 +166,11 @@ export default function Header() {
     setUserPage(isUserPage);
 
   }, [pathname]);
+
+  //          effect: login user가 변경될 때 마다 실행될 함수          //
+  useEffect(() => {
+    setLogin(loginUser !== null);
+  }, [loginUser])
 
   //          render: 헤더 레이아웃 렌더링          //
   return (
