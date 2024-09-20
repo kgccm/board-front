2f80938a4d83f2c4e4d1351a7ae0c647579b1282
KimJiHoon <kgccm8480@gmail.com>
2024-09-20 22:18:34 +0900

    f51

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   apps : [{
     name: 'howse-react',
-    script: 'http-server',
-    args: 'build -p 3000 --proxy http://43.203.242.199:4000? --spa',
-    cwd: '/home/ubuntu/board-front'  // 현재 프론트엔드 프로젝트 경로
+    script: 'index.js',
+    watch: '.'
   }, {
     script: './service-worker/',
     watch: ['./service-worker']
@@ -18,7 +17,7 @@ module.exports = {
       path : 'DESTINATION_PATH',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'pre-setup': '' 
     }
   }
 };
